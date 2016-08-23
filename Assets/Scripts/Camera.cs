71e3befb11746572f5a681872e1fc62d7e65a4ab
Jorge Exequiel Gómez <gomezjorge.exequiel@gmail.com>
2016-08-23 11:02:10 -0300

    Skybox

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class Camera : MonoBehaviour {
 
-    private enum Direction {
-        LEFT, RIGHT
-    }
-
-    //Camera rotating values
-    public float rotationSpeed;
-    private bool rotating = false;
-    private Direction direction;
-
-
     public GameObject player;
     private Vector3 offset;
 
@@ -21,34 +11,4 @@ public class Camera : MonoBehaviour {
         transform.LookAt(player.transform);
     }
 
-    void Update() {
-        //if (Input.GetButton("Shift") && !rotating) {
-        //    if (Input.GetAxis("Horizontal") > 0 && !rotating) {
-        //        direction = Direction.RIGHT;
-        //        rotating = true;    
-        //    } else if (Input.GetAxis("Horizontal") < 0 && !rotating) {
-        //        direction = Direction.LEFT;
-        //        rotating = true;
-        //    }
-        //}
-        //if (rotating) {
-            
-        //}
-    }
-	
-	//void LateUpdate () {
- //       transform.position = player.transform.position + offset;
-	//}
-
-    private void rotateCamera(Direction direction) {
-        switch(direction) {
-            case Direction.LEFT:
-                transform.Translate(Vector3.left * Time.deltaTime);
-                transform.LookAt(player.transform);
-                break;
-            case Direction.RIGHT:
-
-                break;
-        }
-    }
 }
