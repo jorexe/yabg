89d59195ec7eeb336fe885ff478f1be782c5829c
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2016-09-20 20:46:33 -0300

    +Added fan model #Trying to display texture on paper ball mesh

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fan : MonoBehaviour {
+
+    public float force;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
