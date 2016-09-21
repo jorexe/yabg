4f4fb0a9319ada3b9af6e1c300f3e6ea7d78bf55
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2016-09-21 10:04:13 -0300

    +Added scoring

---
+++ Assets/Scripts/Controllers/GameViewController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameViewController : MonoBehaviour {
+
+    private static GameViewController instance;
+
+    public static GameViewController Instance { get { return instance; } }
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            instance = this;
+        }
+        //DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public int score = 10000;
+    public float decreaseRate = 0.2f;
+    public bool playing = true;
+
+    public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        StartGame();
+	}
+
+	void StartGame() {
+        playing = true;
+        StartCoroutine("DecreaseScore");
+    }
+
+    public void IncreaseScore(int amount) {
+        score += amount;
+        scoreText.text = score.ToString();
+    }
+
+    IEnumerator DecreaseScore() {
+        while (playing) {
+            if (score > 0) {
+                score -= 1;
+                scoreText.text = score.ToString();
+            }
+            yield return new WaitForSeconds(decreaseRate);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScoreItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreItem : MonoBehaviour {
+
+    public int scoreAmount;
+
+    void OnTriggerEnter(Collider other) {
+        MainSphere sphere = other.gameObject.GetComponent<MainSphere>();
+        if (sphere != null) {
+            GameViewController.Instance.IncreaseScore(scoreAmount);
+            Destroy(this.gameObject);
+        }
+    }
+}
