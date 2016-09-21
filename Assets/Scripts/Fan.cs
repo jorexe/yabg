0cec1bc2fbb727023bcfef8ccb77cba361964681
Jorge Gómez <gomezjorge.exequiel@gmail.com>
2016-09-21 00:01:59 -0300

    #Updated prefabs #Updated fan script

--- Assets/Scripts/Fan.cs
+++ Assets/Scripts/Fan.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class Fan : MonoBehaviour {
 
     public float force;
-    public float distance;
+    public GameObject rotator;
+    public int rotationSpeed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update() {
+        rotator.transform.Rotate(new Vector3(0f, rotationSpeed, 0f));
+    }
+
+	void OnTriggerStay (Collider other) {
+        Sphere sphere = other.gameObject.GetComponent<Sphere>();
+        if (sphere != null) {
+            sphere.rb.AddForce(new Vector3(0f, force, 0f));
+        }
+    }
 }
--- Assets/Scripts/Models/PaperMaterial.cs
+++ Assets/Scripts/Models/PaperMaterial.cs
@@ -6,9 +6,9 @@ public class PaperMaterial : SphereMaterial {
 
     public override float Drag { get { return 1.0f; } }
 
-    public override float AngularDrag { get { return 0.4f; } }
+    public override float AngularDrag { get { return 0.2f; } }
 
-    public override float Mass { get { return 0.25f; } }
+    public override float Mass { get { return 0.2f; } }
 
     public override float Speed { get { return 4f; } }
 
